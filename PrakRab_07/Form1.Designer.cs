b0e9eb71b7706f63a1ddd8fc8ac47b0d7c06de5f
Akset045 <Akset045@gmail.com>
2021-02-28 21:23:19 +0300

    Add project files.

---
+++ PrakRab_07/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace PrakRab_07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxEncodeKeyWord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDecodeKeyWord = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button1.Location = new System.Drawing.Point(41, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button2.Location = new System.Drawing.Point(245, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(38, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ключевое слово:";
+            // 
+            // textBoxEncodeKeyWord
+            // 
+            this.textBoxEncodeKeyWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBoxEncodeKeyWord.Location = new System.Drawing.Point(41, 76);
+            this.textBoxEncodeKeyWord.Name = "textBoxEncodeKeyWord";
+            this.textBoxEncodeKeyWord.Size = new System.Drawing.Size(151, 27);
+            this.textBoxEncodeKeyWord.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label2.Location = new System.Drawing.Point(242, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ключевое слово:";
+            // 
+            // textBoxDecodeKeyWord
+            // 
+            this.textBoxDecodeKeyWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBoxDecodeKeyWord.Location = new System.Drawing.Point(245, 76);
+            this.textBoxDecodeKeyWord.Name = "textBoxDecodeKeyWord";
+            this.textBoxDecodeKeyWord.Size = new System.Drawing.Size(151, 27);
+            this.textBoxDecodeKeyWord.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 310);
+            this.Controls.Add(this.textBoxDecodeKeyWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxEncodeKeyWord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "3ИП-1-11-18 / Аксенов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxEncodeKeyWord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDecodeKeyWord;
+    }
+}
+
